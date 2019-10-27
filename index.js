1f82f8e78b7d4049541eb6c7ed5219ead9a5ada3
Ubuntu <ubuntu@ip-172-31-40-175.ec2.internal>
2019-10-27 14:28:28 +0000

    audio functions

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+class AudioController {
+    constructor() {
+        this.music = new Audio('assets/audio/cartoon_music.mp3');
+        this.flipAudio = new Audio('assets/audio/card_flip.wav');
+        this.matchAudio = new Audio('assets/audio/card_match.mp3');
+        this.victoryAudio = new Audio('assets/audio/winner.wav');
+        this.gameOverAudio = new Audio('assets/audio/game_over.wav');
+        this.music.volume = 0.3;
+        this.music.loop = true;
+    }
+    startMusic() {
+        this.music.play();
+    }
+    stopMusic() {
+        this.music.pause();
+        this.music.currentTime = 0;
+    }
+    flip() {
+        this.flipAudio.play();
+    }
+    match() {
+        this.matchAudio.play();
+    }
+    victory() {
+        this.stopMusic();
+        this.victoryAudio.play();
+    }
+    gameOver() {
+        this.stopMusic();
+        this.gameOverAudio.play();
+    }
+}
+
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready)
 } else {
